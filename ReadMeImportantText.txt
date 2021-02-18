4ceae19568f0158d1fefa721f1a688f27d4a021f
Urand1503 <59353483+Urand1503@users.noreply.github.com>
2021-02-18 00:57:40 -0600

    init second migration

---
+++ ReadMeImportantText.txt
@@ -0,0 +1,29 @@
+name = await contract.name()
+name
+
+contract.address
+
+truffle console
+contract = await nameOfContract.deployed()
+contract
+
+
+truffle migrate
+
+
+git clone "https address of git to clone" nameofProject
+cd nameOfProject
+npm install
+npm install --save identicon.js@2.3.3
+
+npm run start - starts the server and leave that console open and well as gnashe
+
+
+look at Binance smart chain
+
+.exit 
+
+
+balance.string()
+
+truffle migrate --reset
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const EthSwap = artifacts.require("EthSwap");
 // migrations to put the projetc on the blockchain
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+const MockDaiToken = artifacts.require("MockDaiToken");
+module.exports = async function(deployer) {
+deployer.deploy(MockDaiToken);
+const token = await MockDaiToken.deployed();
+deployer.deploy(EthSwap);
+const ethSwap = await EthSwap.deployed();
+await token.transfer(ethSwap.address, '10000000000000000000')
 };
